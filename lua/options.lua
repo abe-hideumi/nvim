fdc6c94463d255f5b0270400d9ad8ae010d5b7ac
うみ <umi@habe-MacBook-Pro.local>
2026-08-26 22:00:58 +0900

    chore: options.luaにコメントアウト追加

--- lua/options.lua
+++ lua/options.lua
@@ -1,14 +1,19 @@
 local opt = vim.opt
 
+-- Leader key の設定
 vim.g.mapleader = " "
 
+-- 行番号の表示
 opt.number = true
 
+-- タブとインデントの設定
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 
+-- 検索時の大文字・小文字の扱い
 opt.ignorecase = true
 opt.smartcase = true
 
+-- OSのクリップボードを使用
 opt.clipboard = "unnamedplus"
